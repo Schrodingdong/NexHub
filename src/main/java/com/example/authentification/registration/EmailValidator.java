bf13c82094cb454b547373e04acf9cdb90a19073
Calculuxx <aitboumaima@gmail.com>
2023-04-12 21:49:56 +0000

    tried-doing-a-verifying-email-thing

---
+++ src/main/java/com/example/authentification/registration/EmailValidator.java
@@ -0,0 +1,13 @@
+package com.example.authentification.registration;
+
+import org.springframework.stereotype.Service;
+
+import java.util.function.Predicate;
+@Service
+public class EmailValidator implements Predicate<String> {
+    @Override
+    public boolean test(String s){
+        //TODO:Regex
+                return true;
+    }
+}
